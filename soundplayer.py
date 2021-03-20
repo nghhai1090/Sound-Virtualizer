f595e1eafee0d90c51fb91d19c7eac50e7e3f124
nghhai1090 <76409868+nghhai1090@users.noreply.github.com>
2021-03-20 12:02:14 +0100

    Add files via upload

--- soundplayer.py
+++ soundplayer.py
@@ -15,8 +15,6 @@ class virtualizer(object):
         self.canvas.pack()
         self.root.after(0, self.animation)
         self.root.mainloop()
-        
-
      def animation(self):
         track = 0
         CHUNK =1024
@@ -25,31 +23,27 @@ class virtualizer(object):
         wf = wave.open(filename)
         w= wave.open(filename)
         p=pyaudio.PyAudio()
-        #stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,frames_per_buffer=CHUNK)#test mic
+        #stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,frames_per_buffer=CHUNK)# get sound from mic
         stream=p.open(format=p.get_format_from_width(wf.getsampwidth()),  channels = wf.getnchannels(),rate=wf.getframerate(),output=True)
         peak=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
         a= "a"
         while a!= "nan" :
-        #print(wf.getframerate())
-        #while True:
-            #datas=stream.read(CHUNK)
+        #print(wf.getframerate())# get sound from mic
+        #while True:# get sound from mic
+            #datas=stream.read(CHUNK)# get sound from mic
             datas= wf.readframes(CHUNK)
             datab =  np.fromstring(datas,dtype=np.int16)
             peak1=   np.average(np.abs(datab))
             a= str((20*peak1/2**14))
-            #if(a!= "nan" ):all= "#"*int((20*peak1/2**14))
-            #self.canvas.delete(item)
             if(a!='nan' ):
                 peak.pop(0)
                 peak.append(peak1) 
                 for i in range(50):
                     self.canvas.coords(self.items[i],10+i*10,375,20+i*10,375-10*int((20*peak[i]/2**14)))
                 self.canvas.update()
-            stream.write(datas)
-            
+            stream.write(datas)  
         else : print("finish")
         stream.stop_stream()
         stream.close()
         p.terminate()
-
 virtualizer()
